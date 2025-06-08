ba2af2571a8049abcc0a20937086ed424c71efdb
Goferov <marcingodfryd@gmail.com>
2025-06-08 22:41:03 +0200

    Modify bingo only for one user and events

--- database/seeders/BingoGameSeeder.php
+++ database/seeders/BingoGameSeeder.php
@@ -24,41 +24,41 @@ class BingoGameSeeder extends Seeder
             'password' => bcrypt('admin123'),
         ]);
 
-        $users = User::factory(4)->create();
-
+//        $users = User::factory(4)->create();
+//
         $events = Event::factory(50)->create();
-
-        GameSetting::create([
-            'grid_size' => 5,
-            'updated_by_user_id' => $admin->id,
-        ]);
-
-        $game = DailyGame::factory()->create([
-            'date' => now()->format('Y-m-d'),
-        ]);
-
-        foreach ($users as $user) {
-            $card = BingoCard::create([
-                'user_id' => $user->id,
-                'daily_game_id' => $game->id,
-            ]);
-
-            $gridSize = 5;
-            $selectedEvents = $events->random($gridSize * $gridSize)->values();
-            $i = 0;
-
-            for ($row = 0; $row < $gridSize; $row++) {
-                for ($col = 0; $col < $gridSize; $col++) {
-                    BingoTile::create([
-                        'bingo_card_id' => $card->id,
-                        'row_index' => $row,
-                        'col_index' => $col,
-                        'event_id' => $selectedEvents[$i]->id,
-                        'marked' => false,
-                    ]);
-                    $i++;
-                }
-            }
-        }
+//
+//        GameSetting::create([
+//            'grid_size' => 5,
+//            'updated_by_user_id' => $admin->id,
+//        ]);
+//
+//        $game = DailyGame::factory()->create([
+//            'date' => now()->format('Y-m-d'),
+//        ]);
+//
+//        foreach ($users as $user) {
+//            $card = BingoCard::create([
+//                'user_id' => $user->id,
+//                'daily_game_id' => $game->id,
+//            ]);
+//
+//            $gridSize = 5;
+//            $selectedEvents = $events->random($gridSize * $gridSize)->values();
+//            $i = 0;
+//
+//            for ($row = 0; $row < $gridSize; $row++) {
+//                for ($col = 0; $col < $gridSize; $col++) {
+//                    BingoTile::create([
+//                        'bingo_card_id' => $card->id,
+//                        'row_index' => $row,
+//                        'col_index' => $col,
+//                        'event_id' => $selectedEvents[$i]->id,
+//                        'marked' => false,
+//                    ]);
+//                    $i++;
+//                }
+//            }
+//        }
     }
 }
